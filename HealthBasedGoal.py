61f158e9d282a57401c8737d406548bb80c24a68
Pragya Rachur <pragya.rachur@gmail.com>
2019-02-24 17:54:12 -0500

    added output for image based goal

--- HealthBasedGoal.py
+++ HealthBasedGoal.py
@@ -1,11 +1,18 @@
 from Goal import Goal
 from cardio import Cardio
 from strength import Strength
+from random import randint
 
 
 class HealthBasedGoal(Goal):
 
     def __init__(self):
         super(HealthBasedGoal, self).__init__()
-        self.execution_plan.extend([Cardio(cardioType='Treadmill'), Strength(exerciseType='Deadlifts')]) # instances of exercises to do
+        num = randint(1,3)
+        if num == 1:
+            self.execution_plan.extend([Cardio(cardioType='Treadmill'), Strength(exerciseType='Deadlifts')]) # instances of exercises to do
+        elif num == 2:
+            self.execution_plan.extend([Cardio(cardioType='Elliptical'), Strength(exerciseType='Free weights')])
+        elif num == 3:
+            self.execution_plan.extend([Cardio(cardioType='Bike'), Strength(exerciseType='Bicep Curls')])
         return
--- ImageBasedGoal.py
+++ ImageBasedGoal.py
@@ -1,9 +1,11 @@
 from Goal import Goal
 from strength import Strength
+from random import randint
 
 class ImageBasedGoal(Goal):
 
     def __init__(self):
         super(ImageBasedGoal, self).__init__()
-        self.execution_plan.extend([Strength()]) # instances of exercises to do
+        self.execution_plan.extend([Strength(exerciseType='Bicep Curls'), Strength(exerciseType='Squats'),
+                                    Strength(exerciseType='Leg press'), Strength(exerciseType='Russian Twists')])
         return
