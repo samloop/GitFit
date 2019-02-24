6dce099410542ed1c87e8f37923435cd6ad73886
scoleman01 <35472548+scoleman01@users.noreply.github.com>
2019-02-24 16:48:43 -0500

    Add super call

--- WeightBasedGoal.py
+++ WeightBasedGoal.py
@@ -1,7 +1,11 @@
 from Goal import Goal
+from cardio import Cardio
+from strength import Strength
 
 
 class WeightBasedGoal(Goal):
 
     def __init__(self):
+        super(WeightBasedGoal, self).__init__()
+        self.execution_plan.extend([Cardio(cardioType='Elliptical')])
         return
