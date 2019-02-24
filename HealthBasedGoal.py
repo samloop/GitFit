47fcec0c574395d990cf11d126db6f2e85e7237e
scoleman01 <35472548+scoleman01@users.noreply.github.com>
2019-02-24 15:44:48 -0500

    Add files via upload

--- HealthBasedGoal.py
+++ HealthBasedGoal.py
@@ -1,7 +1,11 @@
 from Goal import Goal
+from cardio import Cardio
+from strength import Strength
 
 
 class HealthBasedGoal(Goal):
 
     def __init__(self):
+
+        self.execution_plan.extend([Cardio, Strength]) # instances of exercises to do
         return
