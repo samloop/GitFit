00512ee5d8bcf675206f230529c85fe81058d3dd
Pragya Rachur <pragya.rachur@gmail.com>
2019-03-10 15:34:03 -0400

    fixed int issue

--- HealthBasedGoal.py
+++ HealthBasedGoal.py
@@ -11,11 +11,11 @@ class HealthBasedGoal(Goal):
 
         num = input("What intensity level are you feeling today on a scale of 1-3?")
 
-        if num == 1:
+        if num == '1':
             self.execution_plan.extend([Stretch(name='Lunges'), Cardio(duration=25, cardioType='Bike')]) # instances of exercises to do
-        elif num == 2:
+        elif num == '2':
             self.execution_plan.extend([Stretch(name='Crossack squat'), Cardio(duration=30, cardioType='Elliptical')])
-        elif num == 3:
+        elif num == '3':
             self.execution_plan.extend([Stretch(name='Calf raises'), Cardio(duration=35, cardioType='Treadmill')])
 
         return
